db06247823e0f03459dfb4d7d6f83c87f095552a
ThEWoLf882-42 <agimi@student.1337.ma>
2023-07-30 21:25:43 +0100

    init fix

--- sources/cub3D.c
+++ sources/cub3D.c
@@ -6,7 +6,7 @@
 /*   By: agimi <agimi@student.1337.ma>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/28 15:00:25 by agimi             #+#    #+#             */
-/*   Updated: 2023/07/30 10:50:04 by agimi            ###   ########.fr       */
+/*   Updated: 2023/07/30 20:13:22 by agimi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,10 @@ int	main(int ac, char **av)
 	{
 		init(&cub, av[1]);
 		clear_smap(&cub.sma);
+		free(cub.pts->no);
+		free(cub.pts->so);
+		free(cub.pts->we);
+		free(cub.pts->ea);
 	}
 	else
 		ft_putstr_fd("Please Enter a .cub arg (only)\n", 2);
--- sources/init.c
+++ sources/init.c
@@ -6,43 +6,57 @@
 /*   By: agimi <agimi@student.1337.ma>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/28 15:44:41 by agimi             #+#    #+#             */
-/*   Updated: 2023/07/30 16:21:00 by agimi            ###   ########.fr       */
+/*   Updated: 2023/07/30 20:13:37 by agimi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3D.h"
 
-void	init(t_cub *cub, char *av)
+void	init_pts(t_cub *cub)
 {
 	t_pts	pts;
-	t_col	col;
-	t_smap	*sma;
 
-	cub->sma = NULL;
 	cub->pts = &pts;
 	cub->pts->no = NULL;
 	cub->pts->so = NULL;
 	cub->pts->we = NULL;
 	cub->pts->ea = NULL;
+}
+
+void	init_col(t_cub *cub)
+{
+	t_col	col;
+
 	cub->col = &col;
 	cub->col->cei = 0;
 	cub->col->flo = 0;
-	cub->map = NULL;
-	cub->mlx = NULL;
+}
+
+void	init_sma(t_cub *cub, char *av)
+{
+	t_smap	*sma;
+
+	cub->sma = NULL;
 	open_map(cub, av);
 	read_map(cub);
 	clean_map(cub, cub->sma);
 	check_map(cub, cub->sma);
-	printf("no[%s]\nso[%s]\nwe[%s]\nea[%s]\n", cub->pts->no, cub->pts->so, cub->pts->we, cub->pts->ea);
-	printf("f[%x]\nc[%x]\n", cub->col->flo, cub->col->cei);
 	sma = cub->sma;
 	while (sma)
 	{
 		printf("map[%s]\n", sma->s);
 		sma = sma->nxt;
 	}
-	free(cub->pts->no);
-	free(cub->pts->so);
-	free(cub->pts->we);
-	free(cub->pts->ea);
+}
+
+void	init(t_cub *cub, char *av)
+{
+	init_pts(cub);
+	init_col(cub);
+	init_sma(cub, av);
+	cub->map = NULL;
+	cub->mlx = NULL;
+	printf("no[%s]\nso[%s]\nwe[%s]\nea[%s]\n", cub->pts->no, cub->pts->so,
+		cub->pts->we, cub->pts->ea);
+	printf("f[%x]\nc[%x]\n", cub->col->flo, cub->col->cei);
 }
