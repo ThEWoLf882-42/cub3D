12fd59058fe5ffc83c62836ce9d77c19e4aff2fd
fbelahse <belahsenfatimazahrae@gmail.com>
2023-08-23 11:29:40 +0100

    pause

--- sources/get_out.c
+++ sources/get_out.c
@@ -6,7 +6,7 @@
 /*   By: fbelahse <fbelahse@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/28 16:03:53 by agimi             #+#    #+#             */
-/*   Updated: 2023/08/23 11:23:01 by fbelahse         ###   ########.fr       */
+/*   Updated: 2023/08/23 11:29:28 by fbelahse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,5 @@ void	get_out_s(t_cub *cub, char *s)
 	ft_putstr_fd("Error\n", 2);
 	ft_putstr_fd(s, 2);
 	free_all(cub);
-	pause ();
 	exit(1);
 }
