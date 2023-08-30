051e7d8d1594ae67b78dd9c81d4d96e2bd95ca0d
ThEWoLf882-42 <agimi@student.1337.ma>
2023-08-30 15:50:08 +0100

    mouse

--- sources/mouse.c
+++ sources/mouse.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   mouse.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fbelahse <fbelahse@student.42.fr>          +#+  +:+       +#+        */
+/*   By: agimi <agimi@student.1337.ma>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/01 15:49:50 by agimi             #+#    #+#             */
-/*   Updated: 2023/08/26 11:06:59 by fbelahse         ###   ########.fr       */
+/*   Updated: 2023/08/30 15:48:52 by agimi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,8 @@ void	mouse(double x, double y, void *par)
 
 	(void)y;
 	cub = par;
-	mlx_set_mouse_pos(cub->mx->mlx, WIDTH / 2, HEIGHT / 2);
 	mlx_set_cursor_mode(cub->mx->mlx, MLX_MOUSE_HIDDEN);
+	mlx_set_mouse_pos(cub->mx->mlx, WIDTH / 2, HEIGHT / 2);
 	cub->pan += (x - WIDTH / 2) / MSENS;
 	if (cub->pan > 2 * M_PI)
 		cub->pan -= 2 * M_PI;
